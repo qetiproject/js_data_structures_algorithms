57e149be0da848a136155d03589d3a5e46416248
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-22 20:04:06 +0400

    344. Reverse String

---
+++ 344_ReverseString.js
@@ -0,0 +1,20 @@
+var reverseString = function (s) {
+  s = s.split("").reverse().join("");
+};
+
+//  Time: O(N)
+//  Space: O(N)
+function reverseString(str) {
+  // for (let i = 0; i < str.length; i++) {
+  //     const char = str[i]
+  //     res = char + res
+  // }
+  // return res
+
+  return str.split("").reduce((res, char) => {
+    res = char + res;
+    return res;
+  }, "");
+}
+
+reverseString("word");
