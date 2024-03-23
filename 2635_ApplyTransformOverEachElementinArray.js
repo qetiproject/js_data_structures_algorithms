e28426296df321ae13e9e56b1ee27cc4a2eb20bb
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-23 13:48:36 +0400

    2635. Apply Transform Over Each Element in Array

---
+++ 2635_ApplyTransformOverEachElementinArray.js
@@ -0,0 +1,19 @@
+//leetcode.com/problems/apply-transform-over-each-element-in-array/
+
+//  Time: O(N)
+//  Space: O(1)
+https: var map = function (arr, fn) {
+  for (let i = 0; i < arr.length; ++i) {
+    arr[i] = fn(arr[i], i);
+  }
+  return arr;
+};
+
+//  Time: O(N)
+//  Space: O(N)
+var map = function (arr, fn) {
+  return arr.reduce((transformedArr, element, index) => {
+    transformedArr[index] = fn(element, index);
+    return transformedArr;
+  }, []);
+};
