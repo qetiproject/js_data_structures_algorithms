775d240bf0bc92cc4d992d7c4328c09952fe5de6
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-03-23 17:01:30 +0400

    2626. Array Reduce Transformation

---
+++ 2626_ArrayReduceTransformation.js
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/array-reduce-transformation/
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function (nums, fn, init) {
+  let val = init;
+  nums.forEach((num) => {
+    val = fn(val, num);
+  });
+  return val;
+};
