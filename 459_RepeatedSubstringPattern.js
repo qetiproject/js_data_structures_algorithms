04ee41024c4d5ab5e2831442af595b19e2f8c4bd
keti.khetsuriani <keti.khetsuriani@softchef.io>
2024-04-13 18:17:18 +0400

    459. Repeated Substring Pattern

--- 459_RepeatedSubstringPattern.js
+++ 459_RepeatedSubstringPattern.js
@@ -4,7 +4,7 @@
  * @param {string}
  * @return {boolean}
  */
-https: var repeatedSubstringPattern = function (s) {
+var repeatedSubstringPattern = function (s) {
   let ss = s + s;
   return ss.substring(1, ss.length - 1).includes(s);
 };
