ca8e9a36121307138e0ba6881170723c210f60af
qetiproject <ketideveloper@gmail.com>
2024-08-07 11:45:59 +0400

    26_RemoveDuplicatesfromSortedArra

--- 26_RemoveDuplicatesfromSortedArray.js
+++ 26_RemoveDuplicatesfromSortedArray.js
@@ -2,15 +2,27 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function (nums) {
-  j = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] != nums[i - 1]) {
-      nums[j] = nums[i];
-      j++;
-    }
-  }
-  return j;
-};
+// var removeDuplicates = function (nums) {
+//   j = 1;
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] != nums[i - 1]) {
+//       nums[j] = nums[i];
+//       j++;
+//     }
+//   }
+//   return j;
+// };
 
-console.log(removeDuplicates([1, 1, 2]));
+const nums = [2, 3, 4, 6, 4, 1]
+const removeDuplicates = function(nums) {
+  const result = nums.filter((value, index) => nums.indexOf(value) === index)
+  // const newArr = [...new Set(nums)] // none sorted
+  nums.length = 0
+  nums.push(...result)
+  return nums
+}
+
+removeDuplicates(nums)
+
+console.log(nums)
+
